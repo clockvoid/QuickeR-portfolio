f1e30f71bbdf9c5d34da5523cba480350c08afbe
clockvoid <hidenari.t1997nov@gmail.com>
2019-03-19 23:02:55 +0900

    add: description page

---
+++ src/components/Description.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="description">
+    QuickeRは，皆様のバーコードの管理を楽にします．
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+})
+
+export default class Description extends Vue {}
+</script>
+
+<style lang="scss">
+.description {
+  font-size: 2em;
+}
+</style>
