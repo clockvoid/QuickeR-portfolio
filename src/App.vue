3adeb29fe117f4cb5b845e5d47169106af6d4eb2
clockvoid <hidenari.t1997nov@gmail.com>
2019-03-19 23:01:57 +0900

    apply: top page

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id='app'>
+    <Top />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import Top from './components/Top.vue'
+import Description from './components/Description.vue'
 
 @Component({
   components: {
-    HelloWorld
+    Top,
+    Description
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
+@import url('https://fonts.googleapis.com/css?family=Patua+One');
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
