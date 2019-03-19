44d39e6ec7456b6d3d15e0b47aeec97d6b616e27
clockvoid <hidenari.t1997nov@gmail.com>
2019-03-19 23:03:09 +0900

    add: footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<templete>
+  <div>
+  </div>
+</templete>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+})
+
+export default class Footer extends Vue {}
+
+</script>
+
+<style lang="scss">
+</style>
